9230960cd1724ec0a0844730162651af8b85a0bc
giramos <UO202549@uniovi.es>
2024-02-29 13:44:04 +0100

    :two::two:

---
+++ Pruebas/2019-2020_04Mayo_L2/TPP2223P1L4/Ejercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ejercicio2
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palabras = { "uno", "dos", "tres", "cuatro" };
+            int[] enteros = { 1, 2, 3, 4 };
+            (string,int) r = Combi(palabras, enteros);
+            
+                Console.WriteLine(r.Item1 + r.Item2);
+                Console.WriteLine(r.Item1 + r.Item2);
+                Console.WriteLine(r.Item1 + r.Item2);
+                Console.WriteLine(r.Item1 + r.Item2);
+            
+          
+        }
+
+        public static Tuple<R,T> Combi<R,T>(IEnumerable<R> col1, IEnumerable<T> col2)
+        {
+            Tuple<R, T> lista;
+            IEnumerator<R> enumerator1 = col1.GetEnumerator();
+            IEnumerator<T> enumerator2 = col2.GetEnumerator();
+            while(enumerator1.MoveNext() && enumerator2.MoveNext())
+            {
+                lista.Item1[];= enumerator1.Current;
+                lista.Item2 = enumerator2.Current;
+            }
+            return lista;
+        }
+
+    }
+
+}
