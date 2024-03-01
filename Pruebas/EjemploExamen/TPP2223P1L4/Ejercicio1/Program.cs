0f28f8961f6ca52038ecc62236a9a84045906f8e
giramos <UO202549@uniovi.es>
2024-03-01 08:28:48 +0100

    More More & More!

---
+++ Pruebas/EjemploExamen/TPP2223P1L4/Ejercicio1/Program.cs
@@ -0,0 +1,61 @@
+﻿using ListaGenerica;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+
+namespace Ejercicio1
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int>[] arr = {
+                a => a%2==0,
+                a => a.ToString().Length < 2
+            };
+            Cola<int> cola = new (arr);
+            Console.WriteLine(cola.Encolar(2));
+            Console.WriteLine(cola.EstaVacia);
+        }
+    }
+
+    public class Cola<T>: Lista<T>
+    {
+
+        public bool EstaVacia { get
+            {
+                return NumElementos == 0 ? true : false;
+            } }
+        private readonly IEnumerable<Predicate<T>> predicates;
+
+        public Cola(IEnumerable<Predicate<T>> predicates1)
+        {
+            predicates = predicates1;
+        }
+
+        public bool Encolar(T elemento)
+        {
+            bool exito = false;
+            foreach (var predicate in predicates)
+            {
+                if (!predicate(elemento))
+                {
+                    return false;
+                }
+            }
+            Añadir(elemento);
+            return true;
+        }
+
+        public void Desencolar()
+        {
+            var antiguo = GetElemento(0);
+            Borrar(antiguo);
+        }
+
+    }
+
+  
+}
---
+++ Pruebas/EjemploExamen/TPP2223P1L4/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Ejercicio2
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] autorias = new[] { "Amelia", "Amelio", "Faustina", "Faustino", "Gervasia", "Gervasio" };
+            string[] titulos = new[] { "Uno", "Dos", "Tres", "Cuatro", "Cinco" };
+            int[] años = new[] { 2017, 2018, 2019, 2020, 2021, 2022 };
+            
+          
+        }
+
+
+    }
+
+}
