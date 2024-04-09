720660fc8e44544d1e0d7eca103ed45e6ae759cb
giramos <UO202549@uniovi.es>
2024-04-09 18:50:22 +0200

    hecha tarea obligatoria 10

---
+++ Laboratorios/lab09/ObligatoriaSesion10/ObligatoriaSesion10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+
+namespace ObligatoriaSesion10
+{
+    class Program
+    {
+        /*
+         * This program processes Bitcoin value information obtained from the
+         * url https://api.kraken.com/0/public/OHLC?pair=xbteur&interval=5.
+         */
+        static void Main(string[] args)
+        {
+            var data = Utils.GetBitcoinData();
+            int valor = 7000;
+            const int maximoHilos = 50;
+
+
+            //Console.WriteLine(CultureInfo.CurrentCulture);
+            //foreach (var d in data)
+            //    Console.WriteLine(d);
+
+            MostrarLinea(Console.Out, "Num Hilos", "Ticks", "Resultado");
+            for (int numeroHilos = 1; numeroHilos <= maximoHilos; numeroHilos++)
+            {
+                Master master = new Master(data, valor, numeroHilos);
+                DateTime antes = DateTime.Now;
+                var resultado = master.Calcula();
+                DateTime despues = DateTime.Now;
+                MostrarLinea(Console.Out, numeroHilos, (despues - antes).Ticks, resultado);
+
+                //Entre ejecuciones, limpiamos y esperamos.
+                GC.Collect();
+                GC.WaitForFullGCComplete();
+            }
+            Console.ReadLine();
+        }
+
+        static void MostrarLinea(TextWriter stream, string numHilosCabecera, string ticksCabecera, string resultadoCabecera)
+        {
+            stream.WriteLine("{0};{1};{2}", numHilosCabecera, ticksCabecera, resultadoCabecera);
+        }
+
+        static void MostrarLinea(TextWriter stream, int numHilos, long ticks, int resultado)
+        {
+            stream.WriteLine("{0};{1:N0};{2:N2}", numHilos, ticks, resultado);
+        }
+    }
+}
---
+++ Laboratorios/lab09/ObligatoriaSesion10/ObligatoriaSesion10/Worker.cs
@@ -0,0 +1,44 @@
+﻿namespace ObligatoriaSesion10
+{
+    internal class Worker
+    {
+        /// <summary>
+        /// Vector del que vamos a obtener el módulo
+        /// </summary>
+        private BitcoinValueData[] vector;
+        private int valor;
+        private int indiceDesde, indiceHasta;
+
+        /// <summary>
+        /// Resultado del cálculo
+        /// </summary>
+        private int resultado;
+
+        internal int Resultado
+        {
+            get { return this.resultado; }
+        }
+
+        internal Worker(BitcoinValueData[] vector, int valor, int indiceDesde, int indiceHasta)
+        {
+            this.vector = vector;
+            this.valor = valor;
+            this.indiceDesde = indiceDesde;
+            this.indiceHasta = indiceHasta;
+        }
+
+        /// <summary>
+        /// Método que realiza el cálculo
+        /// </summary>
+        internal void Calcular()
+        {
+            this.resultado = 0;
+            for (int i = this.indiceDesde; i <= this.indiceHasta; i++)
+                if (vector[i].Value >= valor)
+                {
+                    this.resultado += 1;
+                }
+                    
+        }
+    }
+}
