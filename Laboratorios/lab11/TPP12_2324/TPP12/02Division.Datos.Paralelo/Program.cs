470c6b45e47bf6a29df82df435f6784ea53eafdc
giramos <UO202549@uniovi.es>
2024-04-23 18:18:59 +0200

    :cap:

---
+++ Laboratorios/lab11/TPP12_2324/TPP12/02Division.Datos.Paralelo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02Division.Datos.Paralelo
+{
+    class Program
+    {
+        static readonly object obj = new object();
+        /// <summary>
+        /// TPL - Task Parallel Library o Librería de Tareas en Paralelo
+        /// Simplifica la paralelización de aplicaciones:
+        /// 
+        /// Escala dinámicamente el nº de hilos en función del nº de CPUs
+        /// 
+        /// Ofrece servicios para la paralelización mediante la división de datos (data parallelism).
+        /// 
+        /// Ofrece servicios para la paralelización mediante tareas independientes (task parallelism).
+        ///
+        /// En este caso se resuelve Division.Datos.Secuencial con TPL (data parallelism)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            string[] ficheros = Directory.GetFiles(@"..\..\..\..\pics", "*.jpg");
+            string nuevaCarpeta = @"..\..\..\..\pics\rotated";
+            Directory.CreateDirectory(nuevaCarpeta);
+            List<int>threadIds = new ();
+            
+            Stopwatch sw = new Stopwatch();
+            int cont = 1;
+
+            // La siguiente tarea se realiza potencialmente en paralelo
+            // Potencialmente se crean tantas tareas como elementos tiene el IEnumerable.
+
+            //ForEach crea potencialmente un hilo por cada elemento de un IEnumerable
+
+            // Recibe la tarea (Action) a ejecutar.
+            sw.Start();
+            Parallel.ForEach(ficheros, fichero => {
+                string nombreFichero = Path.GetFileName(fichero);
+                using (Bitmap bitmap = new Bitmap(fichero))
+                {
+                    //¿Y si hubiera recursos compartidos?
+                    Console.WriteLine("Procesando el fichero \"{0}\" con el hilo ID {1}.", nombreFichero, Thread.CurrentThread.ManagedThreadId);
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    bitmap.Save(Path.Combine(nuevaCarpeta, nombreFichero));
+                }
+                lock(obj)
+                {
+                    if (!threadIds.Contains(Thread.CurrentThread.ManagedThreadId))
+                        threadIds.Add(Thread.CurrentThread.ManagedThreadId);
+                }
+            });            // TPL Espera a que acaben todos los hilos
+            sw.Stop();
+            //EJERCICIO ¿Cómo podríamos mostrar por pantalla el contador de hilos utilizados en el bloque anterior?
+            Console.WriteLine($"Número total de hilos: {threadIds.Count}");
+
+            Console.WriteLine("Tiempo: {0} ms.", sw.ElapsedMilliseconds);
+
+
+            //Existe también un Parallel.For:
+            //https://docs.microsoft.com/es-es/dotnet/api/system.threading.tasks.parallel.for?view=net-6.0#system-threading-tasks-parallel-for(system-int32-system-int32-system-action((system-int32-system-threading-tasks-parallelloopstate)))
+            //que recibiría el índice de inicio y el de final (no se incluiría).
+            // For crea potencialmente un hilo a partir de un índice de de comienzo y final, no incluyendo el final
+
+
+        }
+
+    }
+}
---
+++ Laboratorios/lab11/TPP12_2324/TPP12/03Division.Datos.Paralelo.Locales/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03Procesado.Tareas.Secuencial.Locales
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Se presenta una sobrecarga del método ForEach en la que cada partición genera un resultado local.
+        /// 
+        /// </summary>
+        static void Main()
+        {
+            var vector = GenerarVectorAleatorio(-10, 10, 1000000);
+            int resultadoTotal = 0;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            //<T,S> donde T es el tipo de origen y S es el tipo del resultado local.
+            Parallel.ForEach<int, int>(
+                //IEnumerable<T> a recorrer. Potencialmente un hilo por elemento.
+                vector,
+
+                // Func<S> que inicializa la variable de resultado local para la partición.
+                () => 0, // () => new List<int>() 
+
+                //Func<T,ParallelLoopState, S> que representa el algoritmo a ejecutar.
+                (actual, loopState, resultadoLocal) =>
+                {
+                    resultadoLocal += actual;
+                    return resultadoLocal;
+                },
+
+                //Action<Q> una vez finalizada la partición, ¿qué hacer con el resultado local?
+                resultadoLocalFinal => Interlocked.Add(ref resultadoTotal, resultadoLocalFinal)
+            );
+            sw.Stop();
+
+            Console.WriteLine("El sumatorio total es {0}. Tiempo: {1}.", resultadoTotal, sw.ElapsedMilliseconds);
+
+            //EJERCICIO: FOREACH _> elementos
+            //Impleméntese el cálculo del valor mínimo en el vector anterior, siguiendo los dos enfoques posibles:
+            //  - Empleando resultados parciales/locales.
+            //  - No empleando resultados parciales/locales (téngase en cuenta los posibles recursos compartidos).
+            // Imprímase el tiempo empleado para cada uno de los enfoques.
+
+            var vector1 = GenerarVectorAleatorio(-10, 10, 1000000);
+            int resultadoMinimo = int.MaxValue;
+            Stopwatch sw1 = new Stopwatch();
+
+            sw1.Start();
+
+            foreach (var actual in vector1)
+            {
+                if (actual < resultadoMinimo)
+                    resultadoMinimo = actual;
+            }
+
+            sw1.Stop();
+
+            Console.WriteLine("FOREACH SIN PARALELIZAR: El valor mínimo es {0}. Tiempo: {1} ms.", resultadoMinimo, sw1.ElapsedMilliseconds);
+
+            var vector2 = GenerarVectorAleatorio(-10, 10, 1000000);
+            int resultadoMinimo1 = int.MaxValue;
+            Stopwatch sw2 = new Stopwatch();
+
+            sw2.Start();
+
+            Parallel.ForEach(vector2, actual =>
+            {
+                if (actual < resultadoMinimo1)
+                    resultadoMinimo1 = actual;
+            });
+
+            sw2.Stop();
+
+            Console.WriteLine("FOREACH NORMAL: El valor mínimo es {0}. Tiempo: {1} ms.", resultadoMinimo1, sw2.ElapsedMilliseconds);
+
+            var vector3 = GenerarVectorAleatorio(-10, 10, 1000000);
+            int resultadoMinimo3 = int.MaxValue;
+            Stopwatch sw3 = new Stopwatch();
+
+            sw3.Start();
+
+            Parallel.ForEach<int, int>(
+                vector3,
+                () => 0,
+                (actual, loopState, resultadoLocal) =>
+                {
+                    if (actual < resultadoLocal)
+                        resultadoLocal = actual;
+                    return resultadoLocal;
+                },
+                resultadoLocalFinal =>
+                {
+                    if (resultadoLocalFinal < resultadoMinimo3)
+                        Interlocked.Exchange(ref resultadoMinimo3, resultadoLocalFinal);
+                }
+            );
+
+            sw3.Stop();
+
+            Console.WriteLine("RESULTADOS LOCALES: El valor mínimo es {0}. Tiempo: {1} ms.", resultadoMinimo3, sw3.ElapsedMilliseconds);
+
+
+
+            //EJERCICIO: FOR -> INDICES Y POSICIONES
+            //Impleméntese el ejercicio anterior empleando el For, almacenando la POSICIÓN del valor mínimo.
+            //Debe almacenarse la posición más cercana al inicio del vector que contenga el valor mínimo:
+            //      {4, 5, 6, 1, 4, 5, 6, 1} -> Resultado esperado: 3
+
+            int[] vector4 = { 4, 5, 6, 1, 4, 5, 6, 1 };
+
+            int posMin = 0;
+            int valorMin = vector4[0];
+
+            Parallel.For(1, vector4.Length, i =>
+            {
+                lock (vector4)
+                {
+                    if (vector4[i] < valorMin)
+                    {
+                        valorMin = vector4[i];
+                        posMin = i;
+                    }
+                }
+
+            });
+
+            Console.WriteLine("FOR PARALELO: La posición del valor mínimo es: " + posMin);
+        }
+
+        static int[] GenerarVectorAleatorio(int min, int max, int tam)
+        {
+            Random random = new Random();
+            int[] vector = new int[tam];
+
+            for (int i = 0; i < tam; i++)
+                vector[i] = random.Next(min, max + 1);
+
+            return vector;
+        }
+    }
+}
